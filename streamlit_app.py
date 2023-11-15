84973860baf545e7210500290b35fcd3aaca3c3b
egnofam <58028463+egnofam@users.noreply.github.com>
2023-11-15 14:05:00 +0100

    Update streamlit_app.py
    add emoticone

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 
 import streamlit
 
-streamlit.title('My parents new healty dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title(' 🥣 My parents new healty dinner')
+streamlit.header(' 🥗 Breakfast Menu')
+streamlit.text(' 🐔 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
